59c87d8e8d37d11a408e38e3877896ae5a835657
Muhaafidz Md Saufi <muhaafidz@gmail.com>
2023-03-02 23:34:26 +0800

    implementing layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import DefaultLayout from '@/layouts/DefaultLayout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
+  )
 }
